7446b64b3fcf59eea111825cc22aa7848096b98c
Aléxia Caló <alexiacalovj@outlook.com>
2026-09-24 01:24:31 -0300

    feat(navigation): configuracao do menu lateral com appshell e rotas

---
+++ Navigation/AppShell.xaml.cs
@@ -0,0 +1,22 @@
+﻿using olhuz_desktop.Features.Reading.Views;
+using olhuz_desktop.Features.Profile.Views;
+
+namespace olhuz_desktop.Navigation
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            // NNavegações que não tem botão próprio no menu lateral
+
+            // Rotas da feature de Leituras
+            //Routing.RegisterRoute(nameof(ReadingDetailPage), typeof(ReadingDetailPage));
+
+            // Rotas da feature de Perfil
+            //Routing.RegisterRoute(nameof(EditProfilePage), typeof(EditProfilePage));
+            //Routing.RegisterRoute(nameof(ChangePasswordPage), typeof(ChangePasswordPage));
+        }
+    }
+}
