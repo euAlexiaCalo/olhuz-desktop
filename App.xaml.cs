bc80ccb19e405f7f0f2a310e4dfd6053b9d6359d
Aléxia Caló <alexiacalovj@outlook.com>
2026-09-24 01:25:46 -0300

    build: adiciona configuracoes de plataforma, propriedades e usings globais

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace olhuz_desktop
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Navigation.AppShell();
+        }
+
+        /*protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }*/
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "olhuz_desktop")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "olhuz_desktop.Pages")]
