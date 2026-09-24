a7fca297447a66dddafd8df697a8d6f2a19554e3
Aléxia Caló <alexiacalovj@outlook.com>
2026-09-24 01:24:45 -0300

    config: configuracao inicial da injecao de dependencias no mauiprogram

---
+++ MauiProgram.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+using olhuz_desktop.Features.Home.Views;
+using olhuz_desktop.Features.Reading.Views;
+using olhuz_desktop.Features.Preferences.Views;
+using olhuz_desktop.Features.Profile.Views;
+// using olhuz_desktop.Features.Home.ViewModels;
+// using olhuz_desktop.Features.Reading.ViewModels;
+// using olhuz_desktop.Features.Preferences.ViewModels;
+// using olhuz_desktop.Features.Profile.ViewModels;
+
+namespace olhuz_desktop
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+            // ==========================================
+            // REGISTRO DE INJEÇÃO DE DEPENDÊNCIA
+            // ==========================================
+
+            // Views
+            builder.Services.AddTransient<HomePage>();
+            builder.Services.AddTransient<ReadingHistoryPage>();
+            builder.Services.AddTransient<PreferencesPage>();
+            builder.Services.AddTransient<ProfilePage>();
+
+            return builder.Build();
+        }
+    }
+}
